e038f84bf2242966e6bedc90065a80b9b0c2a020
Shamim.nrl <shamim.nrl1@gmail.com>
2024-02-11 16:03:32 +0600

    add new file

---
+++ dom/index.js
@@ -0,0 +1,8 @@
+// const x = document.getElementsByTagName('h1')
+// console.log(x)
+
+// const allIl =document.getElementById("all-li")
+// console.log(allIl);
+
+// const y = document.getElementsByTagName("li")
+// console.log(y);
